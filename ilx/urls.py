d11dc7a9ce30bec125319d57dcc66db0eaa2593e
Dee68 <ddimie283@gmail.com>
2023-08-08 23:02:13 +0000

    Add corrections to urls.py.

--- ilx/urls.py
+++ ilx/urls.py
@@ -67,4 +67,5 @@ urlpatterns = [
         )
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
     static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 #urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
