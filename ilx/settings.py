9504b4b8f4ff5783fa03470bae900ed9328786bc
slava_kyiv <vyacheslavkondriyanenko@gmail.com>
2023-08-14 15:30:54 +0000

    add host for heroku

--- ilx/settings.py
+++ ilx/settings.py
@@ -28,10 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-viacheslav1510-ilx-5uyd5tglhb0.ws-eu103.gitpod.io']
-
+if DEBUG: 
+    ALLOWED_HOSTS = ['8000-viacheslav1510-ilx-5uyd5tglhb0.ws-eu103.gitpod.io']
+else:
+    ALLOWED_HOSTS = ['https://ilx-3022db9b1ed6.herokuapp.com/', '*']
 
 # Application definition
 
