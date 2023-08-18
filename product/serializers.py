4bdcfd19bd4688d3493b9007393733b17c55a1cd
Dee68 <ddimie283@gmail.com>
2023-08-18 10:33:17 +0000

    Product serializer update.

--- product/serializers.py
+++ product/serializers.py
@@ -17,6 +17,40 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
+
+    seller = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = (
+            'name',
+            'status',
+            'price',
+            'category',
+            'description',
+            'seller',
+            'quantity',
+            'photo1',
+            'photo2',
+            'photo3',
+            'photo4',
+            'photo5',
+            )
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        product = Product.objects.create(
+            name=validated_data['name'],
+            status=validated_data['status'],
+            price=validated_data['price'],
+            category=validated_data['category'],
+            quantity=validated_data['quantity'],
+            seller=user,
+            photo1=validated_data['photo1'],
+            photo2=validated_data['photo2'],
+            photo3=validated_data['photo3'],
+            photo4=validated_data['photo4'],
+            photo5=validated_data['photo5'],
+        )
+        
+        return product
