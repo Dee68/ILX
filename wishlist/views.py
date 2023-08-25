4cdd29cdf0a90c266b46e525ea0d006c85f98933
Dee68 <ddimie283@gmail.com>
2023-08-25 23:34:20 +0000

    Add correction to views.

--- wishlist/views.py
+++ wishlist/views.py
@@ -6,13 +6,13 @@ from .models import WishList
 
 
 class WishListApiView(generics.ListAPIView):
-    permission_classes = (IsAuthenticated,)
+    #permission_classes = (IsAuthenticated,)
     model = WishList
     queryset = WishList.objects.all()
     serializer_class = WishListSerializer
 
 
 class WishListCreateView(generics.CreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    #permission_classes = (IsAuthenticated,)
     model = WishList
     serializer_class = WishListSerializer
