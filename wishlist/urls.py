433c75fdb12cc5101b1a84672fadcfab693ab968
Dee68 <ddimie283@gmail.com>
2023-08-26 14:11:46 +0000

    Wishlist creation removed.

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from wishlist.views import (
-    WishListApiView,
-    WishListCreateView)
+    WishListApiView,)
+    #WishListCreateView)
 
 
 urlpatterns = [
     path('wishlist/', WishListApiView.as_view()),
-    path('wishlist/<int:pk>/create/', WishListCreateView.as_view())
+    #path('wishlist/<int:pk>/create/', WishListCreateView.as_view())
 ]
