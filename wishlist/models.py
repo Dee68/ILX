ed38fe1ca846fc1e4972ba7b30cbc6d6522d6fe3
Dee68 <ddimie283@gmail.com>
2023-08-31 09:17:41 +0000

    Wish list correction.

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,26 +1,25 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 from product.models import Product
-from django.conf import settings
 # Create your models here.
+
+
+User = get_user_model()
 
 
 class WishList(models.Model):
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.SET_NULL,
-        related_name='user',
-        blank=True,
-        null=True)
+        User,
+        on_delete=models.CASCADE,
+        )
     product = models.ForeignKey(
         Product,
-        on_delete=models.SET_NULL,
-        related_name='product',
-        blank=True,
-        null=True
+        on_delete=models.CASCADE,
         )
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name_plural = 'Wish List'
 
     def __str__(self):
-        return f"{self.user.username}'s wish list"
+        return f"{self.user.username}'s wishlist"
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from wishlist import models
 from .serializers import WishListSerializer
 from .models import WishList
 
@@ -10,3 +11,14 @@ class WishListApiView(generics.ListCreateAPIView):
     model = WishList
     queryset = WishList.objects.all()
     serializer_class = WishListSerializer
+
+
+class UserWishListItems(generics.ListAPIView):
+    queryset = models.WishList.objects.all()
+    serializer_class = WishListSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        user_id = self.kwargs['pk']
+        qs = qs.filter(user__id=user_id)
+        return qs
