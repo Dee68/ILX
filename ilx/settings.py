d4d467f874a4ae811ca5396f4659f00ed905a023
Dee68 <ddimie283@gmail.com>
2023-10-07 08:52:10 +0000

    Set allowed origins

--- ilx/settings.py
+++ ilx/settings.py
@@ -91,8 +91,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ilx.wsgi.application'
 
-CORS_ORIGIN_ALLOW_ALL = True
-#CORS_ORIGIN_WHITELIST = ('https://ilx-react.onrender.com',)
+#CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = ('https://ilx-react.onrender.com',)
 
 AUTH_USER_MODEL = 'account.User'
 
